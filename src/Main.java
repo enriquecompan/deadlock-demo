ce953e9c02b7f44327a82cd97046a1ae7bbb56e7
Enrique Compañ <not@set.org>
2025-05-01 22:57:34 -0600

    Add improvement to reporting thread

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 /**
  * The Main class demonstrates a deadlock scenario using two threads and two shared objects.
- *
+ * <p>
  * It simulates a deadlock by having two virtual threads acquire locks on two shared objects
  * (`obj1` and `obj2`) in opposite order. Thread 1 (`t1`) acquires a lock on `obj1` and then attempts
  * to acquire a lock on `obj2`. Thread 2 (`t2`) acquires a lock on `obj2` and then attempts to acquire
  * a lock on `obj1`.
- *
+ * <p>
  * The program illustrates:
  * - Deadlock conditions where two threads are mutually waiting for each other's locks.
  * - Use of virtual threads introduced in recent Java versions to perform concurrent operations.
- *
+ * <p>
  * Key operations:
  * - `Thread.ofVirtual().start()` is used to create and start virtual threads.
  * - `Thread.sleep()` is called within `t1` to delay the thread, increasing the likelihood of a deadlock.
@@ -55,15 +55,20 @@ class Main {
             System.out.println("t2 is done.");
         });
 
-        // An extra thread to report the status of the other threads
+        // An extra thread to report the status of the other threads and detect if a deadlock happened
         Thread.ofVirtual().start(() -> {
             try {
+                // Wait a little bit to ensure the other thread have enough time to do their tasks
                 Thread.sleep(3000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
             System.out.println("t1 state: " + t1.getState());
             System.out.println("t2 state: " + t2.getState());
+
+            if (t1.getState() == Thread.State.BLOCKED && t2.getState() == Thread.State.BLOCKED) {
+                System.out.println("Deadlock detected!");
+            }
         });
 
         // Wait for both threads to finish gracefully (if they don't deadlock)
